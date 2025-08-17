0a9739ced2f3c0ca59464c5fcc37fe41e3ba2de9
rakiful <mr143131@gmail.com>
2025-08-17 23:29:11 +0600

    problem 2 solved

--- script.js
+++ script.js
@@ -15,3 +15,25 @@ function reverseString(string) {
 console.log(reverseString("hello"));
 console.log(reverseString("world"));
 
+//Problem 2
+function countVowels(string) {
+  if (typeof string !== "string") {
+    return "Invalid input";
+  }
+
+  let string2 = string.toLowerCase()
+  let vowels = "aeiou";
+  let count = 0;
+
+  for (let i = 0; i < string2.length; i++) {
+    if (vowels.includes(string2[i])) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+
+console.log(countVowels("programming hero"));
+console.log(countVowels("rakiful"));
